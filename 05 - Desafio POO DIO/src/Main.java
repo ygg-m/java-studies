0894e6b0c388e5746233aa063c16852a68f41158
Ygor Goulart <ygorgm95@gmail.com>
2022-11-30 14:57:21 -0300

    base

---
+++ 05 - Desafio POO DIO/src/Main.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso cursoJava = new Curso();
+        cursoJava.setTitulo("Curso Java");
+        cursoJava.setDescricao("Descrição Curso Java");
+        cursoJava.setCargaHoraria(8);
+
+        Curso cursoJavaScript = new Curso();
+        cursoJavaScript.setTitulo("Curso JavaScript");
+        cursoJavaScript.setDescricao("Descrição Curso JavaScript");
+        cursoJavaScript.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+    }
+}
